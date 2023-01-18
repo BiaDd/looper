2ee18cba28397c931aeb4cad61f38da53e6844ed
Dan Do <dxd3vmb@virginia.edu>
2023-01-18 17:57:43 -0600

    Starting to implement Dao

---
+++ app/src/main/java/bia/dd/looper/AudioRecordDao.kt
@@ -0,0 +1,23 @@
+package bia.dd.looper
+
+import androidx.room.*
+
+@Dao
+interface AudioRecordDao {
+
+    @Query("SELECT * FROM audioRecords")
+    fun getAll() : List<AudioRecord>
+
+    @Insert
+    fun insert(vararg audioRecord: AudioRecord)
+
+    @Delete
+    fun delete(audioRecord: AudioRecord)
+
+    @Delete
+    fun delete(audioRecords: Array<AudioRecord>)
+
+    @Update
+    fun update(audioRecord: AudioRecord)
+
+}
